9a40b0493a6eb54e348220a0ae5b32ce716a774d
aba2s <aba2sndiaye@yahoo.fr>
2023-04-16 19:34:18 +0200

    initiating front-end

--- sigma_app/views.py
+++ sigma_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def landing_page(request):
+    return render(request, 'index.html')
+
+def data_streams(request):
+    return render(request, 'data_streams.html')
--- sigma_project/settings.py
+++ sigma_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'sigma_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
