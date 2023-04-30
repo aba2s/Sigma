b37d2b7e4bad4370e519296210cb7fc7b5a536fe
aba2s <aba2sndiaye@yahoo.fr>
2023-04-30 18:19:32 +0200

    optimizing static files

--- sigma_app/views.py
+++ sigma_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def landing_page(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def data_streams(request):
     batchs = [
@@ -22,7 +22,7 @@ def data_streams(request):
         "Valid Count": 20, "Error Count": 5, "Import Date":	"16-04-2023 08:19",
         "File name": "FreeWheel import - 16042023"}
     ]
-    return render(request, 'data_streams.html', {"batchs": batchs})
+    return render(request, 'imports.html', {"batchs": batchs})
 
 def status_details(request):
     return render(request, 'modal.html')
