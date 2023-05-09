36c87cce3ad41217c4c23be0181ccbf0f0e96a87
aba2s <aba2sndiaye@yahoo.fr>
2023-05-09 22:56:15 +0200

    users accounts in progress

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,14 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
+			password1 = form.cleaned_data.get("password1")
+			password2 = form.cleaned_data.get("password2")
+			if password1 and password2 and password1 != password2:
+				raise form.ValidationError(
+					form.error_messages['password_mismatch'],
+					code='password_mismatch',
+				)
+	
 			messages.success(request, 'Account was created for', username)
 			return redirect('login')
 		else:
@@ -18,22 +26,23 @@ def register(request):
 			return render(request, 'registration/register.html')
 	else:
 		form = RegisterForm()
-
-	context = {
-		'form': form
-	}
-	return render(request, 'registration/register.html', context )
+		context = {
+			'form': form
+		}
+		return render(request, 'registration/register.html', context )
 
 def login_view(request):
 	if request.method == 'POST':
 		username = request.POST.get('username')
 		password = request.POST.get('password')
 		user = authenticate(request, username=username, password=password)
-
 		if user is not None:
 			login(request, user)
 			return redirect('home')
 			# return redirect('user_insertion_orders', pk=request.user.id)
+		else:
+			messages.error(request, 'Your login is wrong')
+			return render(request, 'registration/login.html')
 	context={}
 	return render(request, 'registration/login.html', context)
 
